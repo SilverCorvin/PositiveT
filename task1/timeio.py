0db3a17bc6385c50fbc44f03af57538a40bcc52a
SilverCorvin <Ivan.Povalyaev@gmail.com>
2017-01-29 19:58:39 +0300

    boilerplate for task1 was added

---
+++ task1/timeio.py
@@ -0,0 +1,12 @@
+def validate(string):
+    pass
+
+
+def to_seconds(string):
+    pass
+
+
+if __name__ == '__main__':
+    timestring = '60.5m'
+    if validate(timestring):
+        print(to_seconds(timestring))
