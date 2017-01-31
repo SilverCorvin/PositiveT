da7c185392dad506e8fa5dfd1c5a3e92a7b8bc51
SilverCorvin <Ivan.Povalyaev@gmail.com>
2017-01-31 20:30:55 +0300

    PEP 8

--- task1/timeio.py
+++ task1/timeio.py
@@ -6,27 +6,31 @@ from exceptions import ValidateError
 
 def validate(string):
     ''' Validates string '''
+    if not string:
+        raise ValidateError("String is empty.")
     patterns = (
-        r'^(\d*)([s]*)$',
+        r'^(\d*)([s]?)$',
         r'^(\d*|\d+\.\d+)([mhd]{1})$',
     )
     for pattern in patterns:
         result = re.match(pattern, string)
         if result:
             return result
-    raise ValidateError("String '{}' doesn't match pattern.".format(string))
+    raise ValidateError("String '{}' is invalid.".format(string))
 
 
 def to_seconds(match_result):
     ''' Returns seconds '''
     multipliers = {'d': 86400, 'h': 3600, 'm': 60, 's': 1}
-    if match_result.group(2):
-        return round(float(match_result.group(1))) * multipliers[match_result.group(2)]
-    return int(match_result.group(1))
+    digital_unit = round(float(match_result.group(1)))
+    time_unit = match_result.group(2)
+    if time_unit:
+        return digital_unit * multipliers[time_unit]
+    return digital_unit
 
 
 if __name__ == '__main__':
-    timestring = sys.argv[1] if len(sys.argv) > 1 else '60.5m'
+    timestring = sys.argv[1] if len(sys.argv) > 1 else ''
     validated = validate(timestring)
     if validated:
         print(to_seconds(validated))
